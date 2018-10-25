6bca4dbc8491aa6f4e84f77e46c401453333882c
Vetm <mail@jonashohmann.com>
2018-10-25 15:26:52 +0200

    Auto stash before revert of "Revert "Add course list""

--- backend/service/mycampus-service.js
+++ backend/service/mycampus-service.js
@@ -1,5 +1,6 @@
 const moodle = require('moodle-client');
 const MoodleUser = require('./moodle-scraper-service').MoodleUser;
+const moment = require('moment');
 
 
 module.exports = {
@@ -12,6 +13,9 @@ module.exports = {
       });
 
       const clientInfo = await client.call({wsfunction: 'core_webservice_get_site_info'});
+
+      await getCourseOverview(client, clientInfo.userid);
+
       return {
         fullName: clientInfo.fullname,
         firstName: clientInfo.firstname,
@@ -31,6 +35,47 @@ module.exports = {
     return moodleUser.fetchModules();
   }
 };
+
+
+async function getCourseOverview(client, userId) {
+  const courses = await client.call({
+    wsfunction: 'core_enrol_get_users_courses',
+    method: "POST",
+    args: {'userid': userId}
+  });
+
+  const courseIds = courses.map((course) => {
+    return course.id;
+  }).join(',');
+
+  const courseIdsArray = courses.map((course) => {
+    return course.id;
+  });
+
+  const coursesDetails = await client.call({
+    wsfunction: 'core_course_get_courses_by_field',
+    method: "POST",
+    args: {'field': 'ids', 'value': courseIds}
+  });
+
+  const navOptions = await client.call({
+    wsfunction: 'core_course_get_user_navigation_options',
+    method: "POST",
+    args: {'courseids': courseIdsArray}
+  });
+
+
+  const today = moment().unix();
+
+  courses.forEach((course) => {
+    const navOptions = client.call({
+      wsfunction: 'core_completion_get_course_completion_status',
+      method: "POST",
+      args: {'courseid': course.id, 'userid': userId}
+    }).then(console.log);
+
+  });
+}
 
 // mod_quiz_get_quizzes_by_courses
 // core_completion_get_course_completion_status
