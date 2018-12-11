8e1ed217dbec54b79f8855c96c8ee6eafe0528a7
Svea <svea.horstmann@web.de>
2018-12-11 17:38:07 +0100

    Links

--- src/app/classes/link.ts
+++ src/app/classes/link.ts
@@ -3,12 +3,16 @@ export class Link {
   url: string;
   titel: string;
   description: string;
+  mail: string;
+  mailTitel: string;
 
 
-  constructor(picture: string, url: string, titel: string, description: string) {
+  constructor(picture: string, url: string, titel: string, description: string, mail: string, mailTitel: string) {
     this.picture = picture;
     this.url = url;
     this.titel = titel;
     this.description = description;
+    this.mail = mail;
+    this.mailTitel = mailTitel;
   }
 }
--- src/app/dashboard/links/links.component.ts
+++ src/app/dashboard/links/links.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Link} from "../../classes/link";
+import {Link} from '../../classes/link';
 
 @Component({
   selector: 'dash-links',
@@ -10,21 +10,26 @@ export class LinksComponent implements OnInit {
 
   public mainLinks: Link[] = [
     new Link('dashboard', 'https://mycampus.iubh.de/my/', 'myCampus - Startseite',
-      'Hier findest du alle Materialen für die Module'),
+      'Hier findest du alle Materialen für die Module', '', ''),
     new Link('live_help', 'https://care-fs.iubh.de/de/#', 'Care - Startseite',
-      'Hier gibt es alle Infos zum Studium'),
+      'Hier gibt es alle Infos zum Studium', '', ''),
     new Link('local_library', 'https://care-fs.iubh.de/de/studium/library-and-information-services.php',
-      'Library Information Service', 'Hier geht es zur Büchersuche'),
+      'Library Information Service', 'Hier geht es zur Büchersuche', '', ''),
   ];
   public functionLinks: Link[] = [
     new Link('note',
       'https://care-fs.iubh.de/de/studium/antragsverwaltung.php?p_id=4113&action=addRequestForm&id=10000073',
-      'Antrag auf Einsicht stellen', 'Fülle das Formular aus, um eine Klausur einzusehen'),
+      'Antrag auf Einsicht stellen', 'Fülle das Formular aus, um eine Klausur einzusehen', '', ''),
     new Link('note', 'https://care-fs.iubh.de/de/studium//kursbuchung.php', 'Kursbuchung',
-      'Buche hier direkt deine nächsten Kurse'),
+      'Buche hier direkt deine nächsten Kurse', '', ''),
   ];
   public phoneLinks: Link[] = [
-    new Link('phone', 'tel:+4986519023453', 'Studierendensekretariat', '+49 (0)8651 90234 53'),
+    new Link('phone', 'tel:+4986519023453', 'Studierendensekretariat', '+49 (0)8651 90234 53',
+      'mail:studium@iubh-fernstudium.de', 'studium@iubh-fernstudium.de'),
+    new Link('phone', 'tel:+4986519023428', 'Prüfungsamt', '+49 (0)8651 90234 28',
+      'mail:pruefungsamt@iubh-fernstudium.de', 'pruefungsamt@iubh-fernstudium.de'),
+    new Link('phone', 'tel:+4986519023456', 'Anerkennung', '+49 (0)8651 90234 56',
+      'mail:anerkennung@iubh-fernstudium.de', 'anerkennung@iubh-fernstudium.de'),
     ];
 
   constructor() {
