1cfc535204a68acc67f1efe9bbc179bf4e08fe2c
Svea <svea.horstmann@web.de>
2018-12-12 07:34:42 +0100

    Modulhandbuch aus Kurssuche öffnen

--- src/app/dashboard/courses/courses.component.ts
+++ src/app/dashboard/courses/courses.component.ts
@@ -1,7 +1,8 @@
 import {Component, EventEmitter, Input, OnInit, Output, Pipe, PipeTransform, ViewChild} from '@angular/core';
-import {DataService} from "../../data.service";
-import {MatInput} from "@angular/material";
-import {animate, state, style, transition, trigger, AnimationEvent} from "@angular/animations";
+import {DataService} from '../../data.service';
+import {MatInput} from '@angular/material';
+import {animate, state, style, transition, trigger, AnimationEvent} from '@angular/animations';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'dash-courses',
@@ -36,7 +37,7 @@ export class CoursesComponent implements OnInit {
   public term = '';
   public closed = false;
 
-  constructor(public dataService: DataService) {
+  constructor(public dataService: DataService, private http: HttpClient) {
     this.close = new EventEmitter<any>();
   }
 
@@ -54,6 +55,39 @@ export class CoursesComponent implements OnInit {
       this.onClose();
     }
   }
+
+  openCourseDescription (name, semester) {
+    semester = this.searchSemesterData(name);
+    this.http.get('/kurs/' + name.toLowerCase().replace(' ', '_'))
+      .subscribe(
+        (res) => {
+          window.open('/kurs/' + name.toLowerCase().replace(' ', '-'));
+        }, (err) => {
+          if (semester.toString().includes('Wahlpflichtmodule')) {
+            window.open('https://www.iubh-fernstudium.de/modulhandbuch/bachelor-wirtschaftsinformatik/'
+              + '#semester5');
+          } else {
+            window.open('https://www.iubh-fernstudium.de/modulhandbuch/bachelor-wirtschaftsinformatik/'
+              + '#semester' + semester.replace('. Semester', ''));
+          }
+          console.log('Semester', semester);
+        });
+  }
+  public searchSemesterData(name): any {
+    for (let semester of this.dataService._raw.transcriptOfRecords.tor) {
+      console.log(semester);
+      for (let module of semester.modules) {
+        for (let courses of module.courses) {
+          if (courses.name == name) {
+            console.log(name, semester, 'hat geklappt');
+            return semester;
+          }
+        }
+      }
+    }
+    return;
+  }
+
 
 }
 
